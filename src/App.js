810cf90e3f62b25964879f620871929ce8e8b1fb
OmkarDhekane <omkarjd1212@gmail.com>
2021-02-25 14:01:40 +0530

    Updated!

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Hi This is Omkar!
+          Learn React! app
         </a>
       </header>
     </div>
